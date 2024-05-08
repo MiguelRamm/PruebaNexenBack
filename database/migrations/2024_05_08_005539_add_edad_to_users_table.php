3d3c35722acabdc464aaefeb01d8292f53d86498
Sinthya <sinthya.cecilia@gmail.com>
2024-05-07 19:23:36 -0600

    Se agrega el campo de fecha_nacimineto en ambas tablas: user_domicilio y users

--- database/migrations/2024_05_08_005539_add_edad_to_users_table.php
+++ database/migrations/2024_05_08_005539_add_edad_to_users_table.php
@@ -6,23 +6,55 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
+     /**
+     * Ejecuta las migraciones necesarias para actualizar la estructura de la tabla 'users' y 'user_domicilio'.
+     * 
+     * Agrega una columna 'fecha_nacimiento' a la tabla 'users', permitiendo valores nulos.
+     * Agrega una columna 'fecha_nacimiento' a la tabla 'user_domicilio', permitiendo valores nulos.
+     * 
+     * @return void
      */
+    
     public function up(): void
     {
+        /**
+         * Modifica la tabla Users
+         */
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->date('fecha_nacimiento')->nullable();
+        });
+
+        /**
+         * Modifica la tabla user_domicilio
+         */
+        Schema::table('user_domicilio', function (Blueprint $table) {
+            $table->date('fecha_nacimiento')->nullable();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Ejecuta las migraciones necesarias para deshacer los cambios realizados en la estructura de la tabla 'users' y 'user_domicilio'.
+     * 
+     * Elimina la columna 'fecha_nacimiento' de la tabla 'users'.
+     * Elimina la columna 'fecha_nacimiento' de la tabla 'user_domicilio'.
+     * 
+     * @return void
      */
     public function down(): void
     {
+        /**
+         * Revertir cambios en la tabla 'users'
+         */
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('fecha_nacimiento');
+        });
+
+        /**
+         * Revertir cambios en la tabla 'user_domicilio'
+         */
+
+        Schema::table('user_domicilio', function (Blueprint $table) {
+            $table->dropColumn('fecha_nacimiento');
         });
     }
 };
